26a429aa3f0e29e483ed28216f7f425fea81903f
Matthew de Rooy <matt@derooy.com.au>
2016-12-19 17:55:51 +1000

    work

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# send build artifacts to production server via pki sftp
+# if localhost??
+
+# only codeDeploy user has access to codeDeploy folder
+
+# api call sharlynn.com.au/api/deploy
